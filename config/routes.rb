a3a69ac96587c8233520cb0dbe2923e428020fff
Kosan3 <ichiro.neko.0421@gmail.com>
2020-05-16 11:17:57 +0900

    chapter8 kyouseifinish

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  delete '/login' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
